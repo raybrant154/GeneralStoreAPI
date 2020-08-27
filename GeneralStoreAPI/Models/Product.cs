462389e23988f57cff3e4f188f2396595ee4b622
raybrant154 <raybrant154@gmail.com>
2020-08-27 10:30:04 -0400

    Initial Commit: Data Model Defined

---
+++ GeneralStoreAPI/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeneralStoreAPI.Models
+{
+    public class Product
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public int NumberInInventory { get; set; }
+        public bool IsInStock { get; set; }
+    }
+}
