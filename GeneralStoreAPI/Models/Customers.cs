5ed07d2a8a941f9a343f4191fd276d39081a6942
raybrant154 <raybrant154@gmail.com>
2020-08-27 11:52:17 -0400

    Entereded database stuff

--- GeneralStoreAPI/Models/Customers.cs
+++ GeneralStoreAPI/Models/Customers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace GeneralStoreAPI.Models
 {
     public class Customer
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string FullName
         {
--- GeneralStoreAPI/Models/Transaction.cs
+++ GeneralStoreAPI/Models/Transaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +9,20 @@ namespace GeneralStoreAPI.Models
 {
     public class Transaction
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [ForeignKey("Customer")]
         public int CustomerId { get; set; }
         public virtual Customer Customer { get; set; }
-        public int ProductId { get; set; }
+        [Required]
+        [ForeignKey("Product")]
+        public int ProductSKU { get; set; }
         public virtual Product Product { get; set; }
+        [Required]
         public int ItemCount { get; set; }
+        [Required]
         public DateTime DateOfTransaction { get; set; }
+
     }
 }
